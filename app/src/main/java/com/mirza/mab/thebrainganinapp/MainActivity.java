c265d3f69b16faf0c3c03b1fe78fac650c671abf
MIRZA AHMED <MIRZA AHMED>
2017-07-01 21:55:31 +0530

    splash screen and basic UI

--- app/src/main/java/com/mirza/mab/thebrainganinapp/MainActivity.java
+++ app/src/main/java/com/mirza/mab/thebrainganinapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.mirza.mab.thebrainganinapp;
 
+import android.app.Activity;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private View baseContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        baseContext=findViewById(R.id.baseContext);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        baseContext.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
     }
 }
