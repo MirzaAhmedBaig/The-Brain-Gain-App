c95e67fdbefcacbd21e506085d56c4074b3d8b84
MIRZA AHMED <MIRZA AHMED>
2017-07-02 14:22:00 +0530

    changes to home screen

--- app/src/main/java/com/mirza/mab/thebrainganinapp/MainActivity.java
+++ app/src/main/java/com/mirza/mab/thebrainganinapp/MainActivity.java
@@ -5,17 +5,21 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     private View baseContext;
+    private Button splay, mplay, settings, share, info, like;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        baseContext=findViewById(R.id.baseContext);
+        baseContext = findViewById(R.id.baseContext);
+        onViewInitialized();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
@@ -29,5 +33,42 @@ public class MainActivity extends Activity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
+    }
+
+    public void onViewInitialized() {
+        splay = (Button) findViewById(R.id.button);
+        mplay = (Button) findViewById(R.id.button2);
+        settings = (Button) findViewById(R.id.button3);
+    }
+
+    public void singlePlay(View v){
+
+    }
+
+    public void multiPlay(View v){
+
+    }
+
+    public void like(View v){
+
+    }
+
+    public void share(View v){
+
+    }
+
+    public void info(View v) {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        baseContext.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        return super.onTouchEvent(event);
     }
 }
